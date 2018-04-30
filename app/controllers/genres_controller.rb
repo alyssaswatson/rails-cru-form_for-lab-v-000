02c3cbcb8cc28be0e95adcfc1e9225fee756aee3
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-04-30 04:25:37 +0000

    automatically backed up by learn

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,22 @@
+class GenresController < ApplicationController
+  def show
+    @genre = Genre.find(params[:id])
+ end
+
+ def new
+   @genre = Genre.new
+ end
+
+ def create
+   @genre = Genre.new(genre_params)
+   @genre.save
+   redirect_to artist_path(@genre)
+end
+
+
+private
+ def genre_params
+     params.require(:genre).permit(:name)
+   end
+
+end
